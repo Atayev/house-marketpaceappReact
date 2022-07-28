59a2bc5f67072ed67516f0aee1e190a1784cc21e
Atayev <atayev25@gmail.com>
2022-07-28 04:31:29 +0400

    added firebase/firestore/auth, added pages, and Signin,signout,profile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Explore from './pages/Explore'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoutes from './components/PrivateRoutes'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore/>} />
+          <Route path='/offers' element={<Offers/>} />
+          <Route path='/profile' element={<PrivateRoutes />}>
+          <Route path='/profile' element={<Profile/>} />
+          </Route>
+          <Route path='/sign-in' element={<Signin/>} />
+          <Route path='/sign-up' element={<Signup/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+        </Routes>
+      <Navbar />
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2N326svKoDy5U6UnUUA4yjv7ZjZnXzpw",
+  authDomain: "house-marketplace-app-ce491.firebaseapp.com",
+  projectId: "house-marketplace-app-ce491",
+  storageBucket: "house-marketplace-app-ce491.appspot.com",
+  messagingSenderId: "909261236411",
+  appId: "1:909261236411:web:037cebf70ff1e03c5f619e",
+  measurementId: "G-Y9M8ZXX4MY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db=getFirestore()
